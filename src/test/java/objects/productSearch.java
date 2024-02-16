28c5418b8f6df637686d521905eba73fb7f90ce2
asmar2000 <M.L.NAGA@student.aast.edu>
2024-02-16 23:25:45 +0200

    removed cart methods into a new class

--- src/test/java/objects/productSearch.java
+++ src/test/java/objects/productSearch.java
@@ -27,23 +27,5 @@ public class productSearch {
         WebElement productImage = driver.findElement(By.id("landingImage"));
         Assert.assertNotNull(productImage, "Image is not displayed"); //verify that the product image is displayed
     }
-    public void AddToCart(){
 
-        WebElement addToCartBTN = driver.findElement(By.id(("add-to-cart-button")));
-        addToCartBTN.click();
-    }
-    public void verifyCart(){
-        Assert.assertTrue(driver.getCurrentUrl().contains("https://www.amazon.eg/cart/"),"URL doesn't contain the expected substring");
-        WebElement cartCount = driver.findElement(By.id("nav-cart-count"));
-        Assert.assertEquals(cartCount.getText(), "1", "Cart does not contain one item");
-
-    }
-    public void removeItem(){
-        WebElement goToCart = driver.findElement(By.id("sw-gtc"));
-        goToCart.click();
-        WebElement deleteBTN = driver.findElement(By.xpath("//input[@value='حذف']"));
-        deleteBTN.click();
-        WebElement cartCountAfterDelete = driver.findElement(By.id("nav-cart-count"));
-        Assert.assertEquals(cartCountAfterDelete.getText(), "0", "Cart is not empty");
-    }
 }
