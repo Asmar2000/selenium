0f4d3ed91fc3a824f5e4ca31afe5d99e52d66af3
asmar2000 <M.L.NAGA@student.aast.edu>
2024-02-16 10:30:06 +0200

    Add productSearch class for searching products

---
+++ src/test/java/Objects/productSearch.java
@@ -0,0 +1,19 @@
+package Objects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+public class productSearch {
+
+    WebDriver driver;
+    public productSearch(WebDriver driver) {
+        this.driver = driver;
+    }
+    By searchBox = By.id("twotabsearchtextbox");
+    By searchResult = By.id("nav-search-submit-button");
+    public void searchFor(String text) {
+        driver.findElement(searchBox).sendKeys(text);
+    }
+    public void searchResult(String text) {
+        driver.findElement(searchResult).sendKeys(text);
+    }
+}
