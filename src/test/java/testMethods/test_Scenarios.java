deb5ac862864504edf0edc19b8b21f2bc23175a2
asmar2000 <M.L.NAGA@student.aast.edu>
2024-02-16 23:27:35 +0200

    Tests after adding 3 different classes

--- src/test/java/testMethods/test_Scenarios.java
+++ src/test/java/testMethods/test_Scenarios.java
@@ -1,6 +1,8 @@
 package testMethods;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import objects.cartItems;
+import objects.todayDeals;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -20,7 +22,6 @@ WebDriver driver;
 
         Dimension taskDimension = new Dimension(1024, 768); //set the window size to 1024 x 768 as mentioned in the task
         driver.manage().window().setSize(taskDimension);
-        driver.get("https://www.amazon.eg/");
     }
     @BeforeMethod
     public void beforeMethod() {
@@ -28,22 +29,29 @@ WebDriver driver;
     }
     @Test
     public void testSearch() {
+        driver.get("https://www.amazon.eg/");
         Assert.assertEquals(driver.getTitle(), "أمازون مصر: تسوق أونلاين | أسعار مخفضة على الإلكترونيات، الأزياء، الموبايل، السوبرماركت والمزيد");
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.amazon.eg/");
-        productSearch page = new productSearch(driver);
-        page.searchFor("car accessories");
-        page.searchResult();
+        productSearch test = new productSearch(driver);
+        cartItems cart = new cartItems(driver);
+        test.searchFor("car accessories");
+        test.searchResult();
         Assert.assertTrue(driver.getCurrentUrl().contains("https://www.amazon.eg/s?k=car+accessories"),"URL doesn't contain the expected substring");
-        page.selectItem();
-        page.productImage();
-        page.AddToCart();
+        test.selectItem();
+        test.productImage();
+        cart.AddToCart();
     }
 
     @Test
     public void verifyCart() {
-        productSearch page = new productSearch(driver);
-        page.verifyCart();
-        page.removeItem();
+        cartItems test = new cartItems(driver);
+        test.verifyCart();
+        test.removeItem();
+    }
+    @Test
+    public void TodayDeals() {
+        todayDeals test = new todayDeals(driver);
+        test.dealItems();
     }
 
 
